b9ae86b2f663e789da000c1ffe1b4a450e1f3e62
vairamuthu <vairamuthu@vairamuthu-VirtualBox.(none)>
2013-07-13 18:39:14 +0530

    first commit

---
+++ lyricslab/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lyricslab::Application.config.secret_key_base = '1332340c594c455c5c1212c998f6d783ef1e520501f35d0823466266e9352435c904e9b50ce1bcf0b33965e27431deeba96ec0bf16b13461c50b0cc201d96205'
---
+++ lyricslab/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lyricslab::Application.config.session_store :cookie_store, key: '_lyricslab_session'
