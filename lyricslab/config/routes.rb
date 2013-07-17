28e80b458c7eb97b236acab510812af67608f929
vairamuthu <vairamuthu.it@gmail.com>
2013-07-17 09:07:58 +0530

    third commit

--- lyricslab/config/routes.rb
+++ lyricslab/config/routes.rb
@@ -1,4 +1,5 @@
 Lyricslab::Application.routes.draw do
+  devise_for :users
   get "home/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
